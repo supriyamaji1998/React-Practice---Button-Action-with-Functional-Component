505b9b6d4c1676b9d1252029d719d80748d4956e
Supriya Maji <78439168+supriyamaji1998@users.noreply.github.com>
2021-08-18 19:56:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,12 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [disp,setDisp]=useState(true);
   return (
     <div id="main">
-      // Do not alter the main div
+    <button id="click" onClick={()=>setDisp(false)}>show me</button>
+    {disp?null:(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+    }
     </div>
   );
 }
